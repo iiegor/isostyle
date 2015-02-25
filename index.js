a013a30d4b8189ccc67ad381f5f24d93ab62d94d
underc0de <iegormundo@hotmail.com>
2015-02-25 16:50:51 +0100

    Compile less files.

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 var path = require('path');
+var through = require('through2');
 var less = require('less');
 
-// Compilation process
+module.exports = function(src) {
+	if (!/\.css$|\.less$/.test(src)) {
+		return through();
+	}
+
+	if (/\.less$/.test(src)) console.info('Compiling: ' + src);
+
+	var buffer = '';
+	var dir = path.dirname(src);
+
+	return through(function(chunk, enc, next) {
+		buffer += chunk.toString();
+		next();
+	}, function(done) {
+		var self = this;
+
+		less.render(buffer, {
+			paths: ['.', dir],
+			compress: true
+		}, function(e, output) {
+			if (e) {
+				var msg = e.message;
+				if (e.line) {
+					msg += ", line " + e.line;
+				}
+				if (e.column) {
+					msg += ", column " + e.column;
+				}
+				if (e.extract) {
+					msg += ": \"" + e.extract + "\"";
+				}
+
+				done(new Error(msg, file, e.line));
+			}
+
+			var compiled = "module.exports = \"" + output.css.replace(/'/g, "\\'").replace(/"/g, '\\"') + "\";";
+
+			self.push(compiled);
+            self.push(null);
+			done();
+		});
+	});
+};
